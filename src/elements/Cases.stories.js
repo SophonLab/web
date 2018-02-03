2d67215838441091cb0cc067147dc2385b452a17
BenCao <zcao@freewheel.tv>
2018-02-03 07:00:03 -0500

    upgrade packages

--- src/elements/Cases.stories.js
+++ src/elements/Cases.stories.js
@@ -5,27 +5,27 @@ import Cases from "./Cases";
 
 const CASES = [
   {
-    "styled":
+    styled:
       "https://images-sophon.s3.amazonaws.com/uploads/pimage/imageurl/4367/thumb400_img17110117334328a592b909.jpg",
-    "origin":
+    origin:
       "https://images-sophon.s3.amazonaws.com/uploads/content/image/2549/thumb400_img.jpg",
-    "style":
+    style:
       "https://images-sophon.s3.amazonaws.com/uploads/style/image/107/thumb200_img.jpg"
   },
   {
-    "styled":
+    styled:
       "https://images-sophon.s3.amazonaws.com/uploads/pimage/imageurl/4363/thumb400_img17103117164679d9c1215c.jpg",
-    "origin":
+    origin:
       "https://images-sophon.s3.amazonaws.com/uploads/content/image/2546/thumb400_img.jpg",
-    "style":
+    style:
       "https://images-sophon.s3.amazonaws.com/uploads/style/image/99/thumb200_img170317024859347d36b6ab.jpeg"
   },
   {
-    "styled":
+    styled:
       "https://images-sophon.s3.amazonaws.com/uploads/pimage/imageurl/4360/thumb400_img171030114611e52a92b4b3.jpg",
-    "origin":
+    origin:
       "https://images-sophon.s3.amazonaws.com/uploads/content/image/2543/thumb400_img.jpg",
-    "style":
+    style:
       "https://images-sophon.s3.amazonaws.com/uploads/style/image/51/thumb200_img.jpg"
   }
 ];
