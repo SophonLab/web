e72d319ba978031ba0dd7593f37b40d8722e796e
BenCao <zcao@freewheel.tv>
2018-02-16 18:51:14 -0500

    native download link

--- src/pages/build/BuildResult.js
+++ src/pages/build/BuildResult.js
@@ -1,6 +1,20 @@
 import React from "react";
 import { Case, Section, SectionSlogan } from "../../elements";
 import { Button } from "antd";
+import { split, last } from "lodash";
+
+export function downloadName(styled, style) {
+  const styledFilename = last(split(styled, '/'));
+
+  const styledExtension = last(split(styledFilename, '.'));
+  const styledName = styledFilename.substring(0, styledFilename.length - styledExtension.length - 1);
+
+  const styleFilename = last(split(style, '/'));
+  const styleExtension = last(split(styleFilename, '.'));
+  const styleName = styleFilename.substring(0, styleFilename.length - styleExtension.length - 1);
+
+  return `${styledName}-${styleName}.${styledExtension}`;
+}
 
 export default function BuildResult({ origin, styled, style, onBuildAgain }) {
   return (
@@ -9,16 +23,16 @@ export default function BuildResult({ origin, styled, style, onBuildAgain }) {
       <Case styled={styled} origin={origin} style={style} />
       <Button.Group style={{ marginTop: "2em" }}>
         <Button
+          htmlType="a"
+          download={downloadName(styled, style)}
+          href={styled}
           type="primary"
           icon="download"
-          onClick={e => {
-            e.preventDefault();
-            window.open(styled);
-          }}
         >
           Download
         </Button>
         <Button
+          htmlType="a"
           onClick={e => {
             e.preventDefault();
             onBuildAgain();
---
+++ src/pages/build/BuildResult.test.js
@@ -0,0 +1,10 @@
+import { downloadName } from "./BuildResult";
+
+test('generate download name', () => {
+  expect(
+    downloadName(
+      'http://abc.com/a/b.png',
+      '/style-images/chinese_flowers_butterfly.jpg'
+    )
+  ).toBe('b-chinese_flowers_butterfly.png');
+});
